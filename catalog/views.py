2b7ef1699582175842217405feb67aaf9fe87be8
Alexandr Sinev <sir.sineow@yandex.ru>
2024-05-05 15:41:21 +0300

    Контроллеры для приложения catalog переписаны с FBV на CBV

--- catalog/views.py
+++ catalog/views.py
@@ -1,13 +1,41 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy, reverse
+from django.views import View
+from django.views.generic import ListView, DetailView, TemplateView
 
 from catalog.models import Product
+
+
+class ProductListView(ListView):
+    model = Product
+
+
+class ProductDetailView(DetailView):
+    model = Product
+
+
+class ContactView(View):
+    model = Product
+
+    def post(self, request):
+        name = request.POST.get('name')
+        phone = request.POST.get('phone')
+        message = request.POST.get('message')
+        print(f'{name} {phone} {message}')
+        return render(request, 'catalog/contacts.html')
+
+    def get(self, request):
+
+        return render(request, 'catalog/contacts.html')
+
+
 
 
 # Create your views here.
 def home_page(request):
     products_list = Product.objects.all()
     context = {'products': products_list}
-    return render(request, 'catalog/home.html', context)
+    return render(request, 'catalog/product_list.html', context)
 
 
 def contacts_page(request):
@@ -22,4 +50,4 @@ def contacts_page(request):
 def product_page(request, pk):
     product = Product.objects.get(pk=pk)
     context = {'product': product}
-    return render(request, 'catalog/product.html', context)
+    return render(request, 'catalog/product_detail.html', context)
