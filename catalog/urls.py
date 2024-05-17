196fc69beafe930f96948778658615438c433a14
Alexandr Sinev <sir.sineow@yandex.ru>
2024-05-17 18:28:13 +0300

    Добавлены url для реализации механизмов CRUD

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,13 +1,15 @@
 from django.urls import path
 
-
 from catalog.apps import CatalogConfig
-from catalog.views import ProductListView, ProductDetailView, ContactView
+from catalog.views import ProductListView, ProductDetailView, ContactView, ProductCreate, ProductUpdate, ProductDelete
 
 app_name = CatalogConfig.name
 
 urlpatterns = [
     path('', ProductListView.as_view(), name='home_page'),
     path('contacts/', ContactView.as_view(), name='contacts'),
-    path('product/<int:pk>/', ProductDetailView.as_view(), name='product')
+    path('product/<int:pk>/', ProductDetailView.as_view(), name='product'),
+    path('create/', ProductCreate.as_view(), name='product_create'),
+    path('edit/<int:pk>/', ProductUpdate.as_view(), name='product_update'),
+    path('delete/<int:pk>/', ProductDelete.as_view(), name='product_delete'),
 ]
